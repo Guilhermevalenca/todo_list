c72c9203fd7d22010d527b444791a0a60b22d597
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-14 21:11:09 -0300

    padronizando axios

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VerificationController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -10,10 +9,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //Arquivos de rotas para api criados dentro do diretorio api_routes, novos arquivos de rotas podem ser adicionados dentros do arquivo api/app/Providers/RouteServiceProvider.php
 
-Route::prefix('email')
-    ->controller(VerificationController::class)
-    ->middleware('auth:sanctum')
-    ->group(function () {
-        Route::get('verify/{id}','verify')->name('verification.verify');
-        Route::get('resend','resend')->name('verification.resend');
-    });
+
--- api/routes/api_routes/UserRoute.php
+++ api/routes/api_routes/UserRoute.php
@@ -3,18 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VerificationController;
 
 Route::prefix('users')
     ->controller(UserController::class)
     ->group(function() {
         Route::post('create','store');
 
+        //autenticando token e revogando token
         Route::controller(AuthController::class)
             ->group(function() {
                 Route::post('login','login');
                 Route::post('logout','logout')->middleware('auth:sanctum');
             });
 
+        //necessita de token para acessar estas funcionalidades
         Route::middleware('auth:sanctum')
             ->group(function() {
                 Route::get('','index');
@@ -23,4 +26,13 @@ Route::prefix('users')
                 Route::put('','update');
                 Route::delete('','delete');
             });
+    });
+
+//verificação de email
+Route::prefix('email')
+    ->controller(VerificationController::class)
+    ->middleware('auth:sanctum')
+    ->group(function () {
+        Route::get('verify/{id}','verify')->name('verification.verify');
+        Route::get('resend','resend')->name('verification.resend');
     });
